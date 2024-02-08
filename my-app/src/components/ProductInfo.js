51dcb1cf8aed87ba954aa006b1c8389d186c12ed
ayseefsin <ayse.efsin@yandex.com>
2024-02-08 13:44:21 +0300

    productDetail component created

--- my-app/src/components/ProductInfo.js
+++ my-app/src/components/ProductInfo.js
@@ -1,41 +1,75 @@
 export const ProductInfo = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-[3vw] text-[#252B42] ">
       <div className="flex gap-[6vw] justify-center leading-6 text-[#737373] mt-[2vw] ">
         <h6 className="font-semibold ">Description</h6>
         <h6 className="font-bold ">Additional Information</h6>
         <h6 className="font-bold ">Reviews (0)</h6>
       </div>
-      <div className="grid grid-cols-3 gap-[2vw] ">
-        <div>
+      <div className="flex gap-[2vw] ml-[6vw]  ">
+        <div className="flex-1 shadow-sm ">
           <img src="/pics/pembeSandalye.png" />
         </div>
-        <div>
-          <h3>başlık</h3>
+        <div className=" flex-1 flex flex-col gap-[2vw] ">
+          <h3 className="font-bold text-2xl">the quick fox jumps over </h3>
           <p>
             Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
             RELIT official consequent door ENIM RELIT Mollie. Excitation venial
-            consequent sent nostrum met. Met minim Mollie non desert Alamo est
-            sit cliquey dolor do met sent. RELIT official consequent door ENIM
-            RELIT Mollie. Excitation venial consequent sent nostrum met. Met
-            minim Mollie non desert Alamo est sit cliquey dolor do met sent.
+            consequent sent nostrum met.
+          </p>
+          <p>
+            Metminim Mollie non desert Alamo est sit cliquey dolor do met sent.
+            RELIT official consequent door ENIM RELIT Mollie. Excitation venial
+            consequent sent nostrum met.
+          </p>
+          <p>
+            Met minim Mollie non desert Alamo est sit cliquey dolor do met sent.
             RELIT official consequent door ENIM RELIT Mollie. Excitation venial
             consequent sent nostrum met.
           </p>
         </div>
-        <div>
+        <div className="flex-1 flex flex-col gap-[2vw] ">
           <div>
-            <h3>başlık</h3>
-            <div>ikon ve yazı</div>
-            <div>ikon ve yazı</div>
-            <div>ikon ve yazı</div>
-            <div>ikon ve yazı</div>
+            <h3 className="font-bold text-2xl">the quick fox jumps over </h3>
           </div>
           <div>
-            <h3>başlık</h3>
-            <div>ikon ve yazı</div>
-            <div>ikon ve yazı</div>
-            <div>ikon ve yazı</div>
+            <div>
+              <div className="flex gap-[2vw] items-center ">
+                <i class="fa-solid fa-chevron-right"></i>
+                <p>the quick fox jumps over the lazy dog</p>
+              </div>
+              <div className="flex gap-[2vw] items-center ">
+                <i class="fa-solid fa-chevron-right"></i>
+                <p>the quick fox jumps over the lazy dog</p>
+              </div>
+              <div className="flex gap-[2vw] items-center ">
+                <i class="fa-solid fa-chevron-right"></i>
+                <p>the quick fox jumps over the lazy dog</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h3 className="font-bold text-2xl">the quick fox jumps over </h3>
+          </div>
+          <div>
+            <div>
+              <div className="flex gap-[2vw] items-center ">
+                <i class="fa-solid fa-chevron-right"></i>
+                <p>the quick fox jumps over the lazy dog</p>
+              </div>
+              <div className="flex gap-[2vw] items-center ">
+                <i class="fa-solid fa-chevron-right"></i>
+                <p>the quick fox jumps over the lazy dog</p>
+              </div>
+              <div className="flex gap-[2vw] items-center ">
+                <i class="fa-solid fa-chevron-right"></i>
+                <p>the quick fox jumps over the lazy dog</p>
+              </div>
+              <div className="flex gap-[2vw] items-center ">
+                <i class="fa-solid fa-chevron-right"></i>
+                <p>the quick fox jumps over the lazy dog</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
