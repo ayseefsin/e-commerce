b745bd5ad75a9050adef045a487e241ce18955ca
ayseefsin <ayse.efsin@yandex.com>
2024-02-28 01:57:04 +0300

    some singUp design fixed

--- my-app/src/page-components/SingUp.js
+++ my-app/src/page-components/SingUp.js
@@ -14,27 +14,48 @@ export const SignUp = () => {
     setPasswordInputValue(event.target.value);
   };
   return (
-    <div>
-      <label>
-        name:
-        <input type="text" value={nameInputValue} onChange={nameInputHandler} />
-      </label>
-      <label>
-        email:
-        <input
-          type="text"
-          value={emailInputValue}
-          onChange={emailInputHandler}
-        />
-      </label>
-      <label>
-        password:
-        <input
-          type="password"
-          value={passwordInputValue}
-          onChange={passwordInputHandler}
-        />
-      </label>
+    <div className=" flex flex-col items-center gap-[2vw] ">
+      <div className="flex gap-[5rem]  ">
+        <label className="text-[2vw]">
+          name:
+          <input
+            className="rounded border border-[#23A6F0]"
+            type="text"
+            value={nameInputValue}
+            onChange={nameInputHandler}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label className="text-[2vw]">
+          email:
+          <input
+            className="rounded border border-[#23A6F0]"
+            type="text"
+            value={emailInputValue}
+            onChange={emailInputHandler}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label className="text-[2vw]">
+          password:
+          <input
+            className="rounded border border-[#23A6F0]"
+            type="password"
+            value={passwordInputValue}
+            onChange={passwordInputHandler}
+          />
+        </label>
+      </div>
+      <button
+        className="bg-[#23A6F0] rounded px-[4vw] py-[2vw] text-[2vw] text-[#ffffff] font-bold "
+        type="submit"
+      >
+        Register
+      </button>
     </div>
   );
 };
