2b1e55db489aca0d8dd9cff957707a0d1749cca9
ayseefsin <ayse.efsin@yandex.com>
2024-03-14 08:02:53 +0300

    router version fixed

--- my-app/src/page-components/SingUp.js
+++ my-app/src/page-components/SingUp.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { set, useForm } from "react-hook-form";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const SignUp = () => {
   const [roles, setRoles] = useState([]);
-  const [role, setRole] = useState(1);
+  const navigate = useNavigate();
 
   const {
     handleSubmit,
@@ -19,10 +21,16 @@ export const SignUp = () => {
       email: "",
       password: "",
       confirmPassword: "",
-      role: null,
+      role_id: 3,
+      store: {
+        name: "",
+        phone: 0,
+        tax_no: "",
+        bank_account: "",
+      },
     },
   });
-  const watchedRole = watch("role");
+  const watchedRole = watch("role_id");
   useEffect(() => {
     const fetchRoles = async () => {
       try {
@@ -40,18 +48,27 @@ export const SignUp = () => {
 
   const onSubmit = async (data) => {
     try {
+      const { confirmPassword, ...postData } = data;
       // Show spinner and disable submit button
       // You can use a loading state to show a spinner
-      console.log(data);
-      const response = await axios.post(
-        "https://workintech-fe-ecommerce.onrender.com/signup",
-        data
-      );
+      console.log(postData);
+      axios
+        .post("https://workintech-fe-ecommerce.onrender.com/signup", postData)
+        .then((response) => {
+          console.log("response", response);
+          toast(response.data.message);
+          reset(); // Reset the form after successful submission
+          navigate(-1);
+        })
+        .catch((error) => {
+          console.log("error", error);
+          toast("Cannot create user!");
+        });
+
       // Redirect user with a message
-      alert("You need to click link in email to activate your account!");
-      reset(); // Reset the form after successful submission
+      // alert("You need to click link in email to activate your account!");
     } catch (error) {
-      console.error("Error submitting form:", error);
+      console.log("Error submitting form:", error);
       // Show error message to the user
       // You can use a state to store error message and display it in the form
     }
@@ -127,7 +144,6 @@ export const SignUp = () => {
         <label>
           Confirm Password:
           <input
-            className="w-[50vw]"
             type="password"
             {...register("confirmPassword", {
               validate: (value) =>
@@ -144,7 +160,7 @@ export const SignUp = () => {
 
         <label>
           Role:
-          <select {...register("role")}>
+          <select {...register("role_id")}>
             {roles.map((r) => (
               <option key={r.id} value={r.id}>
                 {r.name}
@@ -153,14 +169,14 @@ export const SignUp = () => {
           </select>
         </label>
         {watchedRole == 2 && (
-          <>
+          <div className="text-[1.25rem] ml-[2rem] flex flex-col ">
             <label>
               Store Name:
               <input
-                {...register("store_name", { required: true, minLength: 3 })}
+                {...register("store.name", { required: true, minLength: 3 })}
               />
               {errors.store_name && (
-                <span>
+                <span className="text-[red] text-[1rem]">
                   This field is required and must be at least 3 characters
                 </span>
               )}
@@ -168,36 +184,47 @@ export const SignUp = () => {
             <label>
               Store Phone:
               <input
-                {...register("store_phone", {
+                {...register("store.phone", {
                   required: true,
                   pattern: /^[0-9]{10}$/,
                 })}
               />
-              {errors.store_phone && <span>Invalid phone number</span>}
+              {errors.store_phone && (
+                <span className="text-[red] text-[1rem]">
+                  Invalid phone number
+                </span>
+              )}
             </label>
             <label>
               Store Tax ID:
               <input
-                {...register("store_tax_id", {
+                {...register("store.tax_no", {
                   required: true,
                   pattern: /^T\d{4}V\d{6}$/,
                 })}
               />
               {errors.store_tax_id && (
-                <span>Invalid tax ID (format: TXXXXVXXXXXX)</span>
+                <span className="text-[red] text-[1rem]">
+                  Invalid tax ID (format: TXXXXVXXXXXX)
+                </span>
               )}
             </label>
             <label>
               Store Bank Account:
-              <input {...register("store_bank_account", { required: true })} />
-              {errors.store_bank_account && <span>This field is required</span>}
+              <input {...register("store.bank_account", { required: true })} />
+              {errors.store_bank_account && (
+                <span className="text-[red] text-[1rem]">
+                  This field is required
+                </span>
+              )}
             </label>
-          </>
+          </div>
         )}
         <div className="flex items-center">
           <button
+            disabled={!isValid}
             className="bg-main-blue w-[25vw] text-[white] font-bold rounded border-2 border-[#2A7CC7] shadow-lg py-[2vw] m-[2vw] align-middle "
-            type="submit "
+            type="submit"
           >
             Submit
           </button>
