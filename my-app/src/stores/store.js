6184498e7bbe2fa43d1f3155dc49c4d52ccabf6b
ayseefsin <ayse.efsin@yandex.com>
2024-03-26 23:44:22 +0300

    redux continue

--- my-app/src/stores/store.js
+++ my-app/src/stores/store.js
@@ -4,7 +4,14 @@ import {
   combineReducers,
 } from "redux";
 
+import globalReducer from "../reducers/globalReducer";
+import userReducer from "../reducers/userReducer";
+import productReducer from "../reducers/productReducer";
 import thunk from "redux-thunk";
-const rootReducer = combineReducers({ globalReducer });
+const rootReducer = combineReducers({
+  globalReducer,
+  userReducer,
+  productReducer,
+});
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
